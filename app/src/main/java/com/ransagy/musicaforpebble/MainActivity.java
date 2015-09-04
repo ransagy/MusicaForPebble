362f74a05eaff4f8c7c3ade008d93277517b2b7a
Ran Sagy <ransagy@gmail.com>
2015-09-04 13:35:18 +0300

    Removed the pointless Settings menu item.

--- app/src/main/java/com/ransagy/musicaforpebble/MainActivity.java
+++ app/src/main/java/com/ransagy/musicaforpebble/MainActivity.java
@@ -7,12 +7,10 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.content.LocalBroadcastManager;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     private String[] metaArray = null;
 
@@ -49,27 +47,5 @@ public class MainActivity extends ActionBarActivity {
 
         // Register for changes sent from our service.
         LocalBroadcastManager.getInstance(getApplicationContext()).registerReceiver(mServiceDataReceiver, new IntentFilter(MetaChangedService.LOCAL_INTENT));
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
